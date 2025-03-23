0f264134d8030f55d8130ade97846f4fbd34aea4
PhiltonM <philtobeck@gmail.com>
2025-03-23 14:29:25 +0100

    changes

--- code/functions.R
+++ code/functions.R
@@ -1,124 +1,136 @@
-## Function that fits Markov-switching GAMLSS using the msgamboostLSS algorithm
-FitMarkovSwitchingGAMLSS <- function(x,
-                                     y,
-                                     N = 2,
-                                     type = "MSGAMLSS",
-                                     stat = FALSE,
-                                     m.stop = rep(480, 2),
-                                     max.iter = 50,
-                                     conv.tol = 1e-02,
-                                     conv.print = TRUE,
-                                     init_state_trans_prob = NULL,
-                                     formula = NULL) { 
+# Load necessary libraries
+library(dplyr)
 
-  delta = NULL
-  if(is.null(init_state_trans_prob)){
-    gamma = matrix(c(0.5, 0.5, 0.5, 0.5), ncol = 2)
+# Function to fit Markov-switching GAMLSS using the msgamboostLSS algorithm
+FitMarkovSwitchingGAMLSS <- function(x, y, N = 2, type = "MSGAMLSS", stat = FALSE, m.stop = rep(480, 2), max.iter = 50, conv.tol = 1e-02, conv.print = TRUE, init_state_trans_prob = NULL, formula = NULL) {
+  delta <- NULL
+  gamma <- if (is.null(init_state_trans_prob)) {
+    matrix(c(0.5, 0.5, 0.5, 0.5), ncol = 2)
+  } else {
+    as.matrix(init_state_trans_prob)
   }
-  else{
-    gamma = as.matrix(init_state_trans_prob)
+  mod <- fv.mu <- fv.sigma <- list()
+  term <- FALSE
+  old <- 0
+  circ.crit <- 0
+  
+  for (j in 1:N) {
+    fv.mu[[j]] <- rep(c(3, 7)[j], length(y))
+    fv.sigma[[j]] <- rep(3, length(y))
   }
-  mod = fv.mu = fv.sigma = list()
-  term = FALSE
-  old = 0
-  circ.crit = 0
-  for(j in 1:N) {
-    fv.mu[[j]] = rep(c(3, 7)[j], length(y))
-    fv.sigma[[j]] = rep(3, length(y))
-  }
-  while(term == FALSE) {
-    for(i in 1:max.iter) {
-      delta.next = delta
-      gamma.next = gamma
-      fv.mu.next = fv.mu
-      fv.sigma.next = fv.sigma
-      if(is.null(delta)) {
-        delta = solve(t(diag(N) - gamma + 1), rep(1, N))
+  
+  while (term == FALSE) {
+    for (i in 1:max.iter) {
+      delta.next <- delta
+      gamma.next <- gamma
+      fv.mu.next <- fv.mu
+      fv.sigma.next <- fv.sigma
+      
+      if (is.null(delta)) {
+        delta <- solve(t(diag(N) - gamma + 1), rep(1, N))
       }
-      allprobs = matrix(NA, length(y), N)
-      lalpha = lbeta = matrix(NA, N, length(y))
-      for(j in 1:N) {
-        allprobs[,j] = apply(as.matrix(y), 2, dNO, mu = fv.mu.next[[j]], sigma=exp(fv.sigma.next[[j]]))
+      
+      allprobs <- matrix(NA, length(y), N)
+      lalpha <- lbeta <- matrix(NA, N, length(y))
+      
+      for (j in 1:N) {
+        allprobs[, j] <- apply(as.matrix(y), 2, dNO, mu = fv.mu.next[[j]], sigma = exp(fv.sigma.next[[j]]))
       }
-      allprobs = ifelse(!is.na(allprobs), allprobs, 1)
-      foo = delta * allprobs[1,]
-      sumfoo = sum(foo)
-      lscale = log(sumfoo)
-      foo = foo / sumfoo
-      lalpha[, 1] = log(foo) + lscale
-      for(j in 2:length(y)) {
-        foo = foo %*% gamma * allprobs[j,]
-        sumfoo = sum(foo)
-        lscale = lscale + log(sumfoo)
-        foo = foo / sumfoo
-        lalpha[, j] = log(foo) + lscale
+      
+      allprobs <- ifelse(!is.na(allprobs), allprobs, 1)
+      foo <- delta * allprobs[1, ]
+      sumfoo <- sum(foo)
+      lscale <- log(sumfoo)
+      foo <- foo / sumfoo
+      lalpha[, 1] <- log(foo) + lscale
+      
+      for (j in 2:length(y)) {
+        foo <- foo %*% gamma * allprobs[j, ]
+        sumfoo <- sum(foo)
+        lscale <- lscale + log(sumfoo)
+        foo <- foo / sumfoo
+        lalpha[, j] <- log(foo) + lscale
       }
-      foo = rep(1 / N, N)
-      lbeta[, length(y)] = rep(0, N)
-      lscale = log(N)
-      foo = foo / sum(foo)
-      for(j in (length(y) - 1):1) {
-        foo = gamma %*% (allprobs[j + 1,] * foo)
-        lbeta[, j] = log(foo) + lscale
-        sumfoo = sum(foo)
-        foo = foo / sumfoo
-        lscale = lscale + log(sumfoo)
-      }                   
-      lallprobs = log(allprobs)
-      llh = max(lalpha[, length(y)]) + log(sum(exp(lalpha[, length(y)] - max(lalpha[, length(y)]))))
-      weights = matrix(NA, N, length(y))
-      for(j in 1:length(y)) {
-        weights[,j] = exp(lalpha[,j] + lbeta[, j] - llh)
+      
+      foo <- rep(1 / N, N)
+      lbeta[, length(y)] <- rep(0, N)
+      lscale <- log(N)
+      foo <- foo / sum(foo)
+      
+      for (j in (length(y) - 1):1) {
+        foo <- gamma %*% (allprobs[j + 1, ] * foo)
+        lbeta[, j] <- log(foo) + lscale
+        sumfoo <- sum(foo)
+        foo <- foo / sumfoo
+        lscale <- lscale + log(sumfoo)
       }
-      for(j in 1:N) {
-        for(k in 1:N) {
-          gamma.next[j, k] = gamma[j, k] * sum(exp(lalpha[j, 1:(length(y) - 1)] + lallprobs[2:length(y), k] + lbeta[k, 2:length(y)] - llh))
+      
+      lallprobs <- log(allprobs)
+      llh <- max(lalpha[, length(y)]) + log(sum(exp(lalpha[, length(y)] - max(lalpha[, length(y)]))))
+      weights <- matrix(NA, N, length(y))
+      
+      for (j in 1:length(y)) {
+        weights[, j] <- exp(lalpha[, j] + lbeta[, j] - llh)
+      }
+      
+      for (j in 1:N) {
+        for (k in 1:N) {
+          gamma.next[j, k] <- gamma[j, k] * sum(exp(lalpha[j, 1:(length(y) - 1)] + lallprobs[2:length(y), k] + lbeta[k, 2:length(y)] - llh))
         }
       }
-      gamma.next = gamma.next / apply(gamma.next, 1, sum)
-      if(stat == TRUE) {
-        delta.next = solve(t(diag(N) - gamma.next + 1), rep(1, N))
-      }else{
-        delta.next = exp(lalpha[, 1] + lbeta[, 1] - llh)
-        delta.next = delta.next / sum(delta.next)
+      
+      gamma.next <- gamma.next / apply(gamma.next, 1, sum)
+      
+      if (stat == TRUE) {
+        delta.next <- solve(t(diag(N) - gamma.next + 1), rep(1, N))
+      } else {
+        delta.next <- exp(lalpha[, 1] + lbeta[, 1] - llh)
+        delta.next <- delta.next / sum(delta.next)
       }
-      conv.crit = sum(abs(gamma - gamma.next)) + sum(abs(delta - delta.next))
-      ind = weights
-      for(j in 1:N){
-        if(type == "MSGLMLSS") {
-            mod[[j]] = glmboostLSS(formula , weights = ind[j,], data = data.frame(x, y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
+      
+      conv.crit <- sum(abs(gamma - gamma.next)) + sum(abs(delta - delta.next))
+      ind <- weights
+      
+      for (j in 1:N) {
+        if (type == "MSGLMLSS") {
+          mod[[j]] <- glmboostLSS(formula, weights = ind[j, ], data = data.frame(x, y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
         }
-        if(type == "MSGAMLSS") {
-          mod[[j]] = gamboostLSS(formula, weights = ind[j,], data = data.frame(x, y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
+        if (type == "MSGAMLSS") {
+          mod[[j]] <- gamboostLSS(formula, weights = ind[j, ], data = data.frame(x, y), families = as.families("NO"), method = "noncyclic", control = boost_control(mstop = m.stop[j], nu = 0.1))
         }
-        fv.mu.next[[j]] = as.vector(fitted(mod[[j]]$mu))
-        fv.sigma.next[[j]] = as.vector(fitted(mod[[j]]$sigma))
+        fv.mu.next[[j]] <- as.vector(fitted(mod[[j]]$mu))
+        fv.sigma.next[[j]] <- as.vector(fitted(mod[[j]]$sigma))
       }
-      conv.crit = abs(llh - old)
-      if(conv.print == TRUE) {
+      
+      conv.crit <- abs(llh - old)
+      
+      if (conv.print == TRUE) {
         cat("Iteration = ", i, ", criterion = ", round(conv.crit, 3), "\r", sep = "")
       }
-      if(conv.crit < conv.tol | (conv.crit < 1 & abs(conv.crit - circ.crit) < 1e-09) | i == max.iter) {
-        if(i == max.iter) {
+      
+      if (conv.crit < conv.tol | (conv.crit < 1 & abs(conv.crit - circ.crit) < 1e-09) | i == max.iter) {
+        if (i == max.iter) {
           print(paste("No convergence within", max.iter, "iterations"))
-        }else{
+        } else {
           print(paste("Convergence after", i, "iterations"))
         }
-        term = TRUE
+        term <- TRUE
         break
-      }  
-      delta = delta.next
-      gamma = gamma.next
-      fv.mu = fv.mu.next
-      fv.sigma = fv.sigma.next
-      old = llh
+      }
+      
+      delta <- delta.next
+      gamma <- gamma.next
+      fv.mu <- fv.mu.next
+      fv.sigma <- fv.sigma.next
+      old <- llh
       final_i <- i
     }
   }
+  
   return(list(delta = delta.next, gamma = gamma.next, mod = mod, m.stop = m.stop, llh = llh, state.probs = weights, final_i = final_i))
 }
 
-## Function to predict the optimal state sequence using the Viterbi algorithm
+# Function to predict the optimal state sequence using the Viterbi algorithm
 PredictStateSequence <- function(model, y) {
   N <- length(model$delta)
   T <- length(y)
@@ -166,20 +178,15 @@ PredictStateSequence <- function(model, y) {
   return(best_path)
 }
 
-# Load necessary libraries
-library(dplyr)
-
 # Function to compute the likelihood of the test data
 compute_likelihood <- function(test_data, delta, gamma, model) {
   n <- nrow(test_data)
   k <- length(delta)
   mu <- list(exp(fitted(model$mod[[1]]$mu)), fitted(model$mod[[2]]$mu))
   sigma <- list(exp(fitted(model$mod[[1]]$sigma)), exp(fitted(model$mod[[2]]$sigma)))
+  
   # Initialize the likelihood matrix
   likelihood <- matrix(0, n, k)
-  
-  # Transform coefficients into parameters
-
   
   # Compute the initial likelihood
   for (i in 1:k) {
@@ -189,7 +196,7 @@ compute_likelihood <- function(test_data, delta, gamma, model) {
   # Compute the likelihood for the rest of the data
   for (t in 2:n) {
     for (j in 1:k) {
-      likelihood[t, j] <- sum(likelihood[t-1, ] * gamma[, j]) * dnorm(test_data$y[t], mean = mu[[j]][[t]], sd = sigma[[j]][[t]])
+      likelihood[t, j] <- sum(likelihood[t - 1, ] * gamma[, j]) * dnorm(test_data$y[t], mean = mu[[j]][[t]], sd = sigma[[j]][[t]])
     }
   }
   
@@ -208,7 +215,7 @@ cv_msgamlss <- function(x, y, states, init_probs_list, max_iter = 10, mstop = c(
   folds[seq(2, n, by = 2)] <- 2
   mse_list <- list()
   accuracy_list <- list()
-  likelihood_list <- list()  # List to store likelihoods
+  likelihood_list <- list()
   model_results_list <- list()
   final_probs_list <- list()
   final_iterations_list <- list()
@@ -219,7 +226,7 @@ cv_msgamlss <- function(x, y, states, init_probs_list, max_iter = 10, mstop = c(
   for (init_probs in init_probs_list) {
     mse <- numeric(k)
     accuracy <- numeric(k)
-    likelihood <- numeric(k)  # Vector to store likelihoods for each fold
+    likelihood <- numeric(k)
     model_results <- list()
     final_probs <- list()
     final_iterations <- numeric(k)
@@ -263,7 +270,7 @@ cv_msgamlss <- function(x, y, states, init_probs_list, max_iter = 10, mstop = c(
     final_probs_list[[toString(init_probs)]] <- final_probs
     final_iterations_list[[toString(init_probs)]] <- final_iterations
     accuracy_list[[toString(init_probs)]] <- accuracy
-    likelihood_list[[toString(init_probs)]] <- likelihood  # Store likelihoods
+    likelihood_list[[toString(init_probs)]] <- likelihood
   }
   
   return(list(
@@ -271,49 +278,52 @@ cv_msgamlss <- function(x, y, states, init_probs_list, max_iter = 10, mstop = c(
     final_probs = final_probs_list,
     final_iterations = final_iterations_list,
     accuracy = accuracy_list,
-    likelihood = likelihood_list  # Return likelihoods
+    likelihood = likelihood_list
   ))
 }
 
+# Function to process cross-validation results
 cv_results <- function(results, init_state_probs_list, true_gamma, k) {
-    gamma_list <- matrix(NA, length(init_state_probs_list) * k, 5)
-    i <- 1
-    while (i <= length(init_state_probs_list) * k) {
-        for (model in 1:length(init_state_probs_list)) {
-            for (j in 1:k) {
-                gamma_list[i, 1] <- results$model_results[[model]][[j]]$gamma[1, 1]
-                gamma_list[i, 2] <- results$model_results[[model]][[j]]$gamma[1, 2]
-                gamma_list[i, 3] <- results$model_results[[model]][[j]]$gamma[2, 1]
-                gamma_list[i, 4] <- results$model_results[[model]][[j]]$gamma[2, 2]
-                gamma_list[i, 5] <- (abs(gamma_list[i, 1] - true_gamma[1, 1]) + abs(gamma_list[i, 2] - true_gamma[1, 2]) + abs(gamma_list[i, 3] - true_gamma[2, 1]) + abs(gamma_list[i, 4] - true_gamma[2, 2])) / 4
-                i <- i + 1
-            }
-        }
+  gamma_list <- matrix(NA, length(init_state_probs_list) * k, 5)
+  i <- 1
+  
+  while (i <= length(init_state_probs_list) * k) {
+    for (model in 1:length(init_state_probs_list)) {
+      for (j in 1:k) {
+        gamma_list[i, 1] <- results$model_results[[model]][[j]]$gamma[1, 1]
+        gamma_list[i, 2] <- results$model_results[[model]][[j]]$gamma[1, 2]
+        gamma_list[i, 3] <- results$model_results[[model]][[j]]$gamma[2, 1]
+        gamma_list[i, 4] <- results$model_results[[model]][[j]]$gamma[2, 2]
+        gamma_list[i, 5] <- (abs(gamma_list[i, 1] - true_gamma[1, 1]) + abs(gamma_list[i, 2] - true_gamma[1, 2]) + abs(gamma_list[i, 3] - true_gamma[2, 1]) + abs(gamma_list[i, 4] - true_gamma[2, 2])) / 4
+        i <- i + 1
+      }
     }
-    colnames(gamma_list) <- c("gamma_11", "gamma_12", "gamma_21", "gamma_22", "abs_diff")
-    gamma_df <- as.data.frame(gamma_list)
-    gamma_df <- reshape2::melt(gamma_df)
-    
-    results_df <- data.frame(
-        init_state_probs = rep(sapply(init_state_probs_list, function(x) paste(x, collapse = ",")), each = k),
-        gamma_11 = gamma_list[, 1],
-        gamma_12 = gamma_list[, 2],
-        gamma_21 = gamma_list[, 3],
-        gamma_22 = gamma_list[, 4],
-        abs_dif = gamma_list[, 5],
-        accuracy = unlist(results$accuracy),
-        final_i = unlist(results$final_iterations),
-        likelihood = unlist(results$likelihood)  # Add likelihood to results_df
+  }
+  
+  colnames(gamma_list) <- c("gamma_11", "gamma_12", "gamma_21", "gamma_22", "abs_diff")
+  gamma_df <- as.data.frame(gamma_list)
+  gamma_df <- reshape2::melt(gamma_df)
+  
+  results_df <- data.frame(
+    init_state_probs = rep(sapply(init_state_probs_list, function(x) paste(x, collapse = ",")), each = k),
+    gamma_11 = gamma_list[, 1],
+    gamma_12 = gamma_list[, 2],
+    gamma_21 = gamma_list[, 3],
+    gamma_22 = gamma_list[, 4],
+    abs_dif = gamma_list[, 5],
+    accuracy = unlist(results$accuracy),
+    final_i = unlist(results$final_iterations),
+    likelihood = unlist(results$likelihood)
+  )
+  
+  agg_results_df <- results_df %>%
+    group_by(init_state_probs) %>%
+    summarise(
+      mean_accuracy = mean(accuracy),
+      mean_final_i = round(mean(final_i)),
+      mean_abs_diff = mean(abs_dif),
+      mean_likelihood = mean(likelihood)
     )
-    
-    agg_results_df <- results_df %>%
-        group_by(init_state_probs) %>%
-        summarise(
-            mean_accuracy = mean(accuracy),
-            mean_final_i = round(mean(final_i)),
-            mean_abs_diff = mean(abs_dif),
-            mean_likelihood = mean(likelihood)  # Add mean likelihood to aggregated results
-        )
-    
-    list(gamma_df = gamma_df, results_df = results_df, agg_results_df = agg_results_df)
+  
+  list(gamma_df = gamma_df, results_df = results_df, agg_results_df = agg_results_df)
 }
--- code/sim_experiment.R
+++ code/sim_experiment.R
@@ -1,9 +1,4 @@
-# Set the working directory to the project root or appropriate directory
-setwd("c:/Users/philt/OneDrive - stud.uni-goettingen.de/Studium/Master/WiSe 2425/CTA/SensitivityAnalysisMarkovSwitchingGAMLSS")
-
-# Ensure the test directory path is correct
-testthat::test_dir("code")
-
+# Load necessary libraries
 if (!requireNamespace("renv", quietly = TRUE)) {
     install.packages("renv")
 }
@@ -24,72 +19,106 @@ library(ggplot2)
 library(dplyr)
 library(reshape2)
 
+# Source additional R scripts
 source("code/functions.R")
 source("code/sim_data.R")
 
-init_state_probs_list <- list(
+# Define initial state probabilities and true gamma
+init_probs_list <- list(
   matrix(c(0.95, 0.05, 0.05, 0.95), nrow = 2),
-  matrix(c(0.5, 0.5, 0.5, 0.5), nrow = 2))
+  matrix(c(0.85, 0.15, 0.15, 0.85), nrow = 2),
+  matrix(c(0.75, 0.25, 0.25, 0.75), nrow = 2),
+  matrix(c(0.65, 0.35, 0.35, 0.65), nrow = 2),
+  matrix(c(0.55, 0.45, 0.45, 0.55), nrow = 2),
+  matrix(c(0.5, 0.5, 0.5, 0.5), nrow = 2),
+  matrix(c(0.45, 0.55, 0.55, 0.45), nrow = 2),
+  matrix(c(0.35, 0.65, 0.65, 0.35), nrow = 2),
+  matrix(c(0.25, 0.75, 0.75, 0.25), nrow = 2),
+  matrix(c(0.15, 0.85, 0.85, 0.15), nrow = 2),
+  matrix(c(0.05, 0.95, 0.95, 0.05), nrow = 2)
+)
 
 true_gamma <- matrix(c(0.95, 0.05, 0.05, 0.95), nrow = 2)
 k <- 2
-x_lin <- linear_data[,2:101]
+
+# Prepare linear data
+x_lin <- linear_data[, 2:101]
 y_lin <- linear_data$y
 states_lin <- linear_data$states
 data <- data.frame(y = y_lin, x = x_lin, states = states_lin)
-formula <-as.formula(paste("y ~", paste( names(x_lin), collapse = " + ")))
-formula
+formula <- as.formula(paste("y ~", paste(names(x_lin), collapse = " + ")))
 
-cv_linear <- cv_msgamlss(x = x_lin, y=y_lin, init_probs_list = init_state_probs_list, formula = formula, states = states_lin, type ="MSGLMLSS", max_iter = 100, mstop = c(500,500))
+# Perform cross-validation for linear data
+cv_linear <- cv_msgamlss(x = x_lin, y = y_lin, init_probs_list = init_probs_list, formula = formula, states = states_lin, type = "MSGLMLSS", max_iter = 100, mstop = c(500, 500))
 
-x_nonlin <- nonlinear_data[,2:101]
+# Prepare nonlinear data
+x_nonlin <- nonlinear_data[, 2:101]
 y_nonlin <- nonlinear_data$y
 states_nonlin <- nonlinear_data$states
 data_nonlin <- data.frame(y = y_nonlin, x = x_nonlin, states = states_nonlin)
-formula <-as.formula(paste("y ~", paste("bbs(", names(x_nonlin), ")", collapse = " + ")))
-formula
+formula <- as.formula(paste("y ~", paste("bbs(", names(x_nonlin), ")", collapse = " + ")))
 
-cv_nonlinear <- cv_msgamlss(x = x_nonlin, y=y_nonlin, init_probs_list = init_state_probs_list, formula = formula, states = states_nonlin, type ="MSGAMLSS")
+# Perform cross-validation for nonlinear data
+cv_nonlinear <- cv_msgamlss(x = x_nonlin, y = y_nonlin, init_probs_list = init_probs_list, formula = formula, states = states_nonlin, type = "MSGAMLSS", max_iter = 400, mstop = c(160, 160))
 
-
-cv_res_linear <- cv_results(cv_linear, init_state_probs_list, true_gamma, k)
+# Process results for linear data
+cv_res_linear <- cv_results(cv_linear, init_probs_list, true_gamma, k)
 gamma_lin_df <- cv_res_linear$gamma_df
+MAE_lin <- gamma_lin_df[105:130, ]
+gamma_lin_df <- gamma_lin_df[1:104, ]
 results_lin_df <- cv_res_linear$results_df
 agg_results_lin_df <- cv_res_linear$agg_results_df
+
+# Calculate absolute differences
+abs_diff <- numeric(nrow(agg_results_lin_df))
+for (i in 1:nrow(agg_results_lin_df)) {
+  abs_diff[i] <- sum(abs(init_probs_list[[i]][1, 1] - true_gamma[1, 1]), abs(init_probs_list[[i]][1, 2] - true_gamma[1, 2]), abs(init_probs_list[[i]][2, 1] - true_gamma[2, 1]), abs(init_probs_list[[i]][2, 2] - true_gamma[2, 2])) / 4
+}
+abs_diff <- rev(abs_diff)
+agg_results_lin_df$abs_diff <- abs_diff
+
+# Print results for linear data
 print(agg_results_lin_df)
 print(gamma_lin_df)
 print(results_lin_df)
 
-cv_res_nonlinear <- cv_results(cv_nonlinear, init_state_probs_list, true_gamma, k)
+# Process results for nonlinear data
+cv_res_nonlinear <- cv_results(cv_nonlinear, init_probs_list, true_gamma, k)
 gamma_nonlin_df <- cv_res_nonlinear$gamma_df
+MAE_nonlin <- gamma_nonlin_df[105:130, ]
+gamma_nonlin_df <- gamma_nonlin_df[1:104, ]
 results_nonlin_df <- cv_res_nonlinear$results_df
 agg_results_nonlin_df <- cv_res_nonlinear$agg_results_df
+agg_results_nonlin_df$abs_diff <- abs_diff
+
+# Print results for nonlinear data
 print(agg_results_nonlin_df)
 print(gamma_nonlin_df)
 print(results_nonlin_df)
 
+# Plot results for linear data
 ggplot(linear_data, aes(x = X1, y = y, color = as.factor(states))) +
   geom_point() +
-  labs(title = "Linear Data: Y vs. X", x = "X", y = "Y", color = "States") +
+  labs(title = "", x = "X", y = "Y", color = "States") +
   theme_minimal()
 
 linear_data$time <- 1:nrow(data)
 ggplot(linear_data, aes(x = time, y = y)) +
   geom_line(aes(color = as.factor(states), group = 1)) +
   geom_point(aes(color = as.factor(states))) +
-  labs(title = "Linear Data", x = "Time", y = "Y", color = "States") +
+  labs(title = "", x = "Time", y = "Y", color = "States") +
   theme_minimal()
 
-gamma_lin_df$init_state_probs <- rep(sapply(init_state_probs_list, function(x_lin) paste(x_lin, collapse = ",")), each = 10)
+gamma_lin_df$init_state_probs <- rep(sapply(init_probs_list, function(x_lin) paste(x_lin, collapse = ",")), each = k)
 
 ggplot(gamma_lin_df, aes(x = variable, y = value, color = init_state_probs)) +
   geom_point(position = position_jitter(width = 0.2, height = 0)) +
   geom_hline(yintercept = 0.95, linetype = "dashed", color = "#FF6961") +
   geom_hline(yintercept = 0.05, linetype = "dashed", color = "#6A8DA5") +
-  labs( y = "State Transition probabilities", x ="", color = "Initial state transition probs.") +
+  labs(y = "State Transition probabilities", x = "", color = "Initial state transition probs.") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 12, color = c("#FF6961", "#6A8DA5", "#6A8DA5", "#FF6961")),
-        legend.title = element_text(size = 14)) +
+        legend.title = element_text(size = 12)) +
   scale_x_discrete(labels = c(expression(gamma[11]), expression(gamma[12]), expression(gamma[21]), expression(gamma[22])))
 
 ggplot(gamma_lin_df, aes(x = variable, y = value)) +
@@ -102,9 +131,91 @@ ggplot(gamma_lin_df, aes(x = variable, y = value)) +
   scale_x_discrete(labels = c(expression(gamma[11]), expression(gamma[12]), expression(gamma[21]), expression(gamma[22]))) +
   theme(legend.position = "none", axis.text.x = element_text(size = 14))
 
-ggplot(agg_results_lin_df, aes(x = init_state_probs, y = mean_likelihood)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Mean Likelihood for Each Initial State Probability", x = "Initial State Probability", y = "Mean Likelihood") +
+ggplot(agg_results_lin_df, aes(x = init_state_probs, y = mean_accuracy)) +
+  geom_bar(stat = "identity", aes(fill = "Accuracy")) +
+  geom_point(aes(y = abs_diff, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 3) +
+  geom_line(aes(y = abs_diff, group = 1, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 1) +
+  labs(title = "", x = "Initial State Probability", y = "Accuracy / Difference") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(name = "", values = c("Accuracy" = "#6A8DA5")) +
+  scale_color_manual(name = "", values = c("Absolute Difference \nbetween True Gamma \nand Initial guess" = "#FF6961"))
+
+ggplot(agg_results_lin_df, aes(x = init_state_probs, y = mean_abs_diff)) +
+  geom_bar(stat = "identity", aes(fill = "MAE per Parameter")) +
+  geom_point(aes(y = abs_diff, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 3) +
+  geom_line(aes(y = abs_diff, group = 1, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 1) +
+  labs(title = "", x = "Initial State Probability", y = "MAE / Difference") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(name = "", values = c("MAE per Parameter" = "#6A8DA5")) +
+  scale_color_manual(name = "", values = c("Absolute Difference \nbetween True Gamma \nand Initial guess" = "#FF6961"))
+
+ggplot(agg_results_lin_df, aes(x = init_state_probs, y = mean_final_i)) +
+  geom_bar(stat = "identity", fill = "#6A8DA5") +
+  labs(title = "", x = "Initial State Probability", y = "Mean Convergence iteration") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# Plot results for nonlinear data
+ggplot(nonlinear_data, aes(x = X1, y = y, color = as.factor(states))) +
+  geom_point() +
+  labs(title = "", x = "X", y = "Y", color = "States") +
+  theme_minimal()
+
+nonlinear_data$time <- 1:nrow(data)
+ggplot(nonlinear_data, aes(x = time, y = y)) +
+  geom_line(aes(color = as.factor(states), group = 1)) +
+  geom_point(aes(color = as.factor(states))) +
+  labs(title = "", x = "Time", y = "Y", color = "States") +
+  theme_minimal()
+
+gamma_nonlin_df$init_state_probs <- rep(sapply(init_probs_list, function(x_lin) paste(x_lin, collapse = ",")), each = k)
+
+ggplot(gamma_nonlin_df, aes(x = variable, y = value, color = init_state_probs)) +
+  geom_point(position = position_jitter(width = 0.2, height = 0)) +
+  geom_hline(yintercept = 0.95, linetype = "dashed", color = "#FF6961") +
+  geom_hline(yintercept = 0.05, linetype = "dashed", color = "#6A8DA5") +
+  labs(y = "State Transition probabilities", x = "", color = "Initial state transition probs.") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1, size = 12, color = c("#FF6961", "#6A8DA5", "#6A8DA5", "#FF6961")),
+        legend.title = element_text(size = 12)) +
+  scale_x_discrete(labels = c(expression(gamma[11]), expression(gamma[12]), expression(gamma[21]), expression(gamma[22])))
+
+ggplot(gamma_nonlin_df, aes(x = variable, y = value)) +
+  geom_boxplot(aes(fill = variable)) +
+  scale_fill_manual(values = c("#FF6961", "#6A8DA5", "#6A8DA5", "#FF6961")) +
+  geom_hline(yintercept = 0.95, linetype = "dashed", color = "#FF6961") +
+  geom_hline(yintercept = 0.05, linetype = "dashed", color = "#6A8DA5") +
+  labs(x = "", y = "State transition probability") +
+  theme_minimal() +
+  scale_x_discrete(labels = c(expression(gamma[11]), expression(gamma[12]), expression(gamma[21]), expression(gamma[22]))) +
+  theme(legend.position = "none", axis.text.x = element_text(size = 14))
+
+ggplot(agg_results_nonlin_df, aes(x = init_state_probs, y = mean_accuracy)) +
+  geom_bar(stat = "identity", aes(fill = "Accuracy")) +
+  geom_point(aes(y = abs_diff, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 3) +
+  geom_line(aes(y = abs_diff, group = 1, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 1) +
+  labs(title = "", x = "Initial State Probability", y = "Accuracy / Difference") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(name = "", values = c("Accuracy" = "#6A8DA5")) +
+  scale_color_manual(name = "", values = c("Absolute Difference \nbetween True Gamma \nand Initial guess" = "#FF6961"))
+
+ggplot(agg_results_nonlin_df, aes(x = init_state_probs, y = mean_abs_diff)) +
+  geom_bar(stat = "identity", aes(fill = "MAE per Parameter")) +
+  geom_point(aes(y = abs_diff, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 3) +
+  geom_line(aes(y = abs_diff, group = 1, color = "Absolute Difference \nbetween True Gamma \nand Initial guess"), size = 1) +
+  labs(title = "", x = "Initial State Probability", y = "MAE / Difference") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_fill_manual(name = "", values = c("MAE per Parameter" = "#6A8DA5")) +
+  scale_color_manual(name = "", values = c("Absolute Difference \nbetween True Gamma \nand Initial guess" = "#FF6961"))
+
+ggplot(agg_results_nonlin_df, aes(x = init_state_probs, y = mean_final_i)) +
+  geom_bar(stat = "identity", fill = "#6A8DA5") +
+  labs(title = "", x = "Initial State Probability", y = "Mean Convergence iteration") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
 
